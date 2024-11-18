f647be2bb74a5429f660bb6b0c74ea482a46e043
iriquelmee <i.riquelmee@duocuc.cl>
2024-11-18 02:11:19 -0300

    Creacion componente footer, se consume misma data del nav, se agregaan estilos basicos animacion., clases de bootrap realizan responsividad.

---
+++ src/app/components/layouts/shared/footer/footer.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {nav} from '../../../../data/index';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+  footerTitle:string = "DSY2205 Store"
+  footerItems : any[]  | undefined;
+
+  ngOnInit()
+  {
+    console.log("Cargando: app-footer");
+    this.loadNav();
+  }
+
+  loadNav()
+  {
+    try 
+    {
+      if(nav != undefined)
+      {
+        this.footerItems = nav;
+      } 
+    } catch (ex) 
+    {
+      console.log(ex);
+    }
+  }
+
+}
