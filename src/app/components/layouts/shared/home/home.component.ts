2c2d6432dbe21748cb963730c0a91ad803942c56
iriquelmee <i.riquelmee@duocuc.cl>
2024-11-18 10:18:23 -0300

    implementacion componente producto en componente home

--- src/app/components/layouts/shared/home/home.component.ts
+++ src/app/components/layouts/shared/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductComponent } from '../product/product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
