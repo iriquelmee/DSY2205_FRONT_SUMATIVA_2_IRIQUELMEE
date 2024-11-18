c66e5d429bf0b7f1e3265cac7271250fe407e15e
iriquelmee <i.riquelmee@duocuc.cl>
2024-11-18 18:00:09 -0300

    Se agrega carrito component y faq component a rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { LoginComponent } from './components/layouts/shared/login/login.componen
 import { NotFoundComponent } from './components/layouts/shared/not-found/not-found.component';
 import { ProductComponent } from './components/layouts/shared/product/product.component';
 import { RegisterComponent } from './components/layouts/shared/register/register.component';
+import { FaqComponent } from './components/layouts/shared/faq/faq.component';
+import { CartComponent } from './components/layouts/shared/cart/cart.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'/home', pathMatch:'full'},
     {path: 'home',component:HomeComponent},
     {path: 'productos',component:ProductComponent},
+    {path: 'faq',component:FaqComponent},
+    {path: 'cart',component:CartComponent},
     {path: 'login', component:LoginComponent},
     {path: 'register', component:RegisterComponent},
     {path: '**',component:NotFoundComponent}
