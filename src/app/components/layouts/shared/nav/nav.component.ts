420e2c26484351926cc9bcb7128cd19fb452d924
iriquelmee <i.riquelmee@duocuc.cl>
2024-11-18 02:02:59 -0300

    Se crea componente nav para navegacion de la pagina, se agregan estilos basicos y responsivos para todas las pantallas

---
+++ src/app/components/layouts/shared/nav/nav.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import {nav} from '../../../../data/index'
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+export class NavComponent {
+  
+  constructor (_router : RouterLink){
+
+  }
+  
+  navTitle:string = "DSY2205 Store";
+
+  navLinks : any = nav;
+
+
+  ngOnInit()
+  {
+    console.log("Cargando: app-nav ");
+    this.loadNav();
+  }
+
+  loadNav()
+  {
+    try 
+    {
+      if(nav != undefined)
+      {
+        this.navLinks = nav;
+      } 
+    } catch (ex) 
+    {
+      console.log(ex);
+    }
+  }
+
+}
