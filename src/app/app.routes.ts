b523b9640f666a6f909e6e40fcea97515a7e1ac4
iriquelmee <i.riquelmee@duocuc.cl>
2024-11-18 16:36:51 -0300

    Se agrega componente register en rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './components/layouts/shared/home/home.component';
 import { LoginComponent } from './components/layouts/shared/login/login.component';
 import { NotFoundComponent } from './components/layouts/shared/not-found/not-found.component';
 import { ProductComponent } from './components/layouts/shared/product/product.component';
+import { RegisterComponent } from './components/layouts/shared/register/register.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'/home', pathMatch:'full'},
     {path: 'home',component:HomeComponent},
     {path: 'productos',component:ProductComponent},
     {path: 'login', component:LoginComponent},
+    {path: 'register', component:RegisterComponent},
     {path: '**',component:NotFoundComponent}
 ];
