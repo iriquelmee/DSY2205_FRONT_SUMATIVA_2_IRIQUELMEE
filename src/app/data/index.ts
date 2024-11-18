edbe0599ddd9830cf554603d5f5a353d20503cfb
iriquelmee <i.riquelmee@duocuc.cl>
2024-11-18 10:15:59 -0300

    Se agrega data para componente producto

--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -1,4 +1,88 @@
 export const nav = [
   { name: 'Home', href: '/home' },
   { name: 'Login', href: '/login' },
+  { name: 'Productos', href: '/productos' },
+];
+
+export const productos = [
+  {
+    itemId:"",
+    id: 1,
+    nombre: "Monopoly",
+    descripcion: "El clásico juego de compra y venta de propiedades.",
+    precio: 18000,
+    stock: 20,
+    categoria: "Juegos de Mesa",
+    img: 'assets/img/game-1.png'
+  },
+  {
+    itemId:"",
+    id: 2,
+    nombre: "Uno",
+    descripcion: "Un emocionante juego de cartas donde debes deshacerte de todas tus cartas.",
+    precio: 9000,
+    stock: 25,
+    categoria: "Juegos de Cartas",
+    img: 'assets/img/game-2.png'
+  },
+  {
+    itemId:"",
+    id: 3,
+    nombre: "Mesa de Ajedrez",
+    descripcion: "Una mesa de ajedrez de madera con tablero y piezas.",
+    precio: 15000,
+    stock: 10,
+    categoria: "Juegos de Mesa",
+    img: 'assets/img/game-3.png'
+  },
+  {
+    itemId: "",
+    id: 4,
+    nombre: "Ludo Clásico",
+    descripcion: "El tradicional juego de mesa donde los jugadores deben llevar sus fichas a la meta.",
+    precio: 12000,
+    stock: 25,
+    categoria: "Juegos de Mesa",
+    img: 'assets/img/game-4.png'
+  },
+  {
+    itemId: "",
+    id: 5,
+    nombre: "Naipe Español",
+    descripcion: "Juego de cartas tradicional con el diseño clásico español, ideal para brisca, truco y más.",
+    precio: 5000,
+    stock: 50,
+    categoria: "Juegos de Cartas",
+    img: 'assets/img/game-5.png'
+  },
+  {
+    itemId: "",
+    id: 6,
+    nombre: "Naipe Inglés",
+    descripcion: "El clásico mazo de cartas inglés perfecto para juegos como póker, blackjack o solitario.",
+    precio: 6000,
+    stock: 40,
+    categoria: "Juegos de Cartas",
+    img: 'assets/img/game-6.png'
+  },
+  {
+    itemId: "",
+    id: 7,
+    nombre: "Parlante Bluetooth Portatil Beats Pill 2024",
+    descripcion: "Parlante con sonido envolvente y resistencia al agua. Beats",
+    precio: 149990,
+    stock: 10,
+    categoria: "Accesorios",
+    img: 'assets/img/speaker-1.png'
+  },
+  {
+    itemId: "",
+    id: 8,
+    nombre: "Parlante Portátil Bluetooth Bose Soundlink Flex Negro",
+    descripcion: "Parlante con sonido envolvente Bose",
+    precio: 149990,
+    stock: 12,
+    categoria: "Accesorios",
+    img: 'assets/img/speaker-2.png'
+  },
 ];
