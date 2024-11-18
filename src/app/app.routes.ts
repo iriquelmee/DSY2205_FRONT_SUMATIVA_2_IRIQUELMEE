211ba2bf22b460ee8a3a6eac1d7c0a982649b3b1
iriquelmee <i.riquelmee@duocuc.cl>
2024-11-18 02:14:24 -0300

    Configuracion de vistas y ruteo basico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/layouts/shared/home/home.component';
+import { LoginComponent } from './components/layouts/shared/login/login.component';
+import { NotFoundComponent } from './components/layouts/shared/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'/home', pathMatch:'full'},
+    {path: 'home',component:HomeComponent},
+    {path: 'login', component:LoginComponent},
+    {path: '**',component:NotFoundComponent}
+];
