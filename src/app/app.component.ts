4542febf85a7731482a6867abdbf708f811bac3b
iriquelmee <i.riquelmee@duocuc.cl>
2024-11-18 02:17:30 -0300

    implementacion compoenntes: nav ,body y footer en app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BodyComponent } from './components/layouts/shared/body/body.component';
+import { NavComponent } from './components/layouts/shared/nav/nav.component';
+import { FooterComponent } from './components/layouts/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavComponent, BodyComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
   title = 'dsy2205-store';
 }
