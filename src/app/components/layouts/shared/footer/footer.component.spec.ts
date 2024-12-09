71b87ff2324e442b91868c700157f23462035ac4
iriquelmee <i.riquelmee@duocuc.cl>
2024-12-09 00:23:31 -0300

    Fix add validacion carga data test footer

--- src/app/components/layouts/shared/footer/footer.component.spec.ts
+++ src/app/components/layouts/shared/footer/footer.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { nav } from '../../../../data/index';
 import { FooterComponent } from './footer.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,16 +9,19 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FooterComponent]
-    })
-    .compileComponents();
+      imports: [FooterComponent, RouterTestingModule],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should populate navLinks with data from nav', () => {
+    component.loadNav();
+    expect(component.footerItems).toEqual(nav);
   });
+
 });
