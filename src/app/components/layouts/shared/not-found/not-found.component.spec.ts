27287178bfbac80a34ab76d2eaa8cba052791edd
iriquelmee <i.riquelmee@duocuc.cl>
2024-12-08 23:56:48 -0300

    Fix se agrega test unitario redirigir a home  al hacer click

--- src/app/components/layouts/shared/not-found/not-found.component.spec.ts
+++ src/app/components/layouts/shared/not-found/not-found.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NotFoundComponent } from './not-found.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterLink } from '@angular/router';
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
@@ -8,10 +9,11 @@ describe('NotFoundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NotFoundComponent]
-    })
-    .compileComponents();
+      imports: [NotFoundComponent, RouterTestingModule] // Import the component and RouterTestingModule
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +21,10 @@ describe('NotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to home on button click', () => {
+    const button = fixture.nativeElement.querySelector('a');
+    expect(button.getAttribute('routerLink')).toBe('/home');
   });
 });
